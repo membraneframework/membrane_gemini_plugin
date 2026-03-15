5d6fcce9ae2454e05c837ec84f3f1ef450443aea
Jakub Migdał <j.migdal42@gmail.com>
2026-03-15 15:24:45 +0100

    Add documentation for `interrupted?` flag

--- lib/events/response_end.ex
+++ lib/events/response_end.ex
@@ -1,14 +1,13 @@
 defmodule Membrane.Gemini.Events.ResponseEnd do
   @moduledoc """
   Sent by `Membrane.Gemini.Bin` upon model turn completion or interruption.
+
+  Whether or not the turn was interrupted is signaled by the `interrupted?` flag.
   """
 
   @derive Membrane.EventProtocol
 
-  @type t :: %__MODULE__{
-          #  Whether or not the turn was interrupted by the user
-          interrupted?: boolean()
-        }
+  @type t :: %__MODULE__{interrupted?: boolean()}
 
   @enforce_keys [:interrupted?]
 
