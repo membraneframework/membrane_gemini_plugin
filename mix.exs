3c3b9972b9a6f6e5e5107df1892c4f87e9fa66e0
Piotr Szeremeta <piotrek.szeremeta@swmansion.com>
2026-05-07 17:10:04 +0200

    Relax constraints of support dependencies

--- mix.exs
+++ mix.exs
@@ -48,16 +48,15 @@ defmodule Membrane.Gemini.Mixfile do
       {:bandit, "~> 1.5", only: :test},
       {:websock_adapter, "~> 0.5", only: :test},
       {:plug, "~> 1.16", only: :test},
-      {:ex_doc, "~> 0.34", only: :dev, runtime: false},
-      {:dialyxir, "~> 1.4", only: :dev, runtime: false},
-      {:credo, "~> 1.7", only: :dev, runtime: false}
+      {:ex_doc, ">= 0.0.0", only: :dev, runtime: false},
+      {:dialyxir, ">= 0.0.0", only: :dev, runtime: false},
+      {:credo, ">= 0.0.0", only: :dev, runtime: false}
     ]
   end
 
   defp dialyzer() do
     opts = [
-      flags: [:error_handling],
-      plt_add_apps: [:mix, :syntax_tools]
+      flags: [:error_handling]
     ]
 
     if System.get_env("CI") == "true" do
